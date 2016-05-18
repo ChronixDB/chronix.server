ffaf260e1d734914187e09fbac881d24f5309932
Florian Lautenschlager <florian.lautenschlager@qaware.de>
2016-05-18 09:28:30 +0200

    Fixes sonar violation.

--- chronix-server-query-handler/src/main/java/de/qaware/chronix/solr/query/analysis/functions/transformation/Vectorization.java
+++ chronix-server-query-handler/src/main/java/de/qaware/chronix/solr/query/analysis/functions/transformation/Vectorization.java
@@ -30,6 +30,20 @@ import org.apache.commons.lang3.builder.HashCodeBuilder;
  */
 public class Vectorization implements ChronixTransformation<MetricTimeSeries> {
 
+
+    private final float TOLERANCE;
+
+    /**
+     * Constructs the vectorization transformation.
+     * <p>
+     * A typical tolerance value is 0.01f
+     *
+     * @param tolerance the value that is used to decide if a values
+     */
+    public Vectorization(float tolerance) {
+        this.TOLERANCE = tolerance;
+    }
+
     /**
      * Todo: Describe the algorithm, a bit.
      * <p>
@@ -42,21 +56,19 @@ public class Vectorization implements ChronixTransformation<MetricTimeSeries> {
     @Override
     public MetricTimeSeries transform(MetricTimeSeries timeSeries) {
 
-
         int size = timeSeries.size();
-        byte[] use_point = new byte[size];
+        byte[] usePoint = new byte[size];
 
         long[] rawTimeStamps = timeSeries.getTimestampsAsArray();
         double[] rawValues = timeSeries.getValuesAsArray();
 
-        float TOLERANCE = 0.01f;
-        compute(rawTimeStamps, rawValues, use_point, TOLERANCE);
+        compute(rawTimeStamps, rawValues, usePoint, TOLERANCE);
 
         LongList vectorizedTimeStamps = new LongList();
         DoubleList vectorizedValues = new DoubleList();
         for (int i = 0; i < size; i++) {
             //Value is not vectorized
-            if (use_point[i] != 0) {
+            if (usePoint[i] != 0) {
                 vectorizedTimeStamps.add(rawTimeStamps[i]);
                 vectorizedValues.add(rawValues[i]);
             }
@@ -79,44 +91,44 @@ public class Vectorization implements ChronixTransformation<MetricTimeSeries> {
      * </code>
      * Then the distance from C to P = |s|*L.
      */
-    private double get_distance(long p_x, double p_y, long a_x, double a_y, long b_x, double b_y) {
+    private double getDistance(long pX, double pY, long aX, double aY, long bX, double bY) {
 
-        double l_2 = (b_x - a_x) * (b_x - a_x) + (b_y - a_y) * (b_y - a_y);
-        double s = ((a_y - p_y) * (b_x - a_x) - (a_x - p_x) * (b_y - a_y)) / (l_2);
+        double l_2 = (bX - aX) * (bX - aX) + (bY - aY) * (bY - aY);
+        double s = ((aY - pY) * (bX - aX) - (aX - pX) * (bY - aY)) / (l_2);
 
         return Math.abs(s) * Math.sqrt(l_2);
     }
 
-    private void compute(long[] timestamps, double[] values, byte[] use_point, float tolerance) {
+    private void compute(long[] timestamps, double[] values, byte[] usePoint, float tolerance) {
 
         // do not simplify if not at least 3 points are available
         if (timestamps.length == 3) {
             return;
         }
 
-        int ix_a = 0;
-        int ix_b = 1;
-        use_point[ix_a] = 1;
-        use_point[ix_b] = 1;
+        int ixA = 0;
+        int ixB = 1;
+        usePoint[ixA] = 1;
+        usePoint[ixB] = 1;
         for (int i = 2; i < timestamps.length; i++) {
-            double dist = get_distance(timestamps[i], values[i], timestamps[ix_a], values[ix_a], timestamps[ix_b], values[ix_b]);
+            double dist = getDistance(timestamps[i], values[i], timestamps[ixA], values[ixA], timestamps[ixB], values[ixB]);
             if (dist < tolerance) {
-                use_point[i - 1] = 0;
-                use_point[i] = 1;
+                usePoint[i - 1] = 0;
+                usePoint[i] = 1;
             } else {
 
                 // do not continue if not at least one more point is available
                 if (i + 1 >= timestamps.length) {
                     for (int j = i; j < timestamps.length; j++) {
-                        use_point[j] = 1;
+                        usePoint[j] = 1;
                     }
                     return;
                 }
 
-                ix_a = i;
-                ix_b = i + 1;
-                use_point[ix_a] = 1;
-                use_point[ix_b] = 1;
+                ixA = i;
+                ixB = i + 1;
+                usePoint[ixA] = 1;
+                usePoint[ixB] = 1;
                 i++; // continue with next point
             }
         }
