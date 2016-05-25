c4a0e2215af3e16642d85c45b4b0f9850ffe93ac
Florian Lautenschlager <florian.lautenschlager@qaware.de>
2016-05-25 10:16:40 +0200

    Fixes sonar violations.

--- chronix-server-query-handler/src/main/java/de/qaware/chronix/solr/query/analysis/functions/math/NElements.java
+++ chronix-server-query-handler/src/main/java/de/qaware/chronix/solr/query/analysis/functions/math/NElements.java
@@ -112,9 +112,13 @@ public final class NElements {
 
         @Override
         public boolean equals(Object o) {
-            if (this == o) return true;
+            if (this == o){
+                return true;
+            }
 
-            if (o == null || getClass() != o.getClass()) return false;
+            if (o == null || getClass() != o.getClass()){
+                return false;
+            }
 
             Pair pair = (Pair) o;
 
