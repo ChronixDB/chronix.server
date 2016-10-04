2c11eed206ff28e0e6ac5a2ec46a3b703d740429
Florian Lautenschlager <florian.lautenschlager@qaware.de>
2016-10-04 17:06:00 +0200

    Skeleton for the ingestion handler.

--- chronix-server-ingestion-handler/src/main/java/de/qaware/chronix/solr/ingestion/ChronixIngestionHandler.java
+++ chronix-server-ingestion-handler/src/main/java/de/qaware/chronix/solr/ingestion/ChronixIngestionHandler.java
@@ -15,7 +15,11 @@
  */
 package de.qaware.chronix.solr.ingestion;
 
+import de.qaware.chronix.converter.BinaryTimeSeries;
+import de.qaware.chronix.converter.KassiopeiaSimpleConverter;
+import de.qaware.chronix.timeseries.MetricTimeSeries;
 import org.apache.commons.io.IOUtils;
+import org.apache.solr.common.SolrInputDocument;
 import org.apache.solr.core.PluginInfo;
 import org.apache.solr.core.SolrCore;
 import org.apache.solr.handler.RequestHandlerBase;
@@ -25,6 +29,8 @@ import org.apache.solr.util.plugin.PluginInfoInitialized;
 import org.apache.solr.util.plugin.SolrCoreAware;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.time.Instant;
 
 /**
  * The Chronix ingestion handler
@@ -43,9 +49,31 @@ public class ChronixIngestionHandler extends RequestHandlerBase implements SolrC
     public void handleRequestBody(SolrQueryRequest req, SolrQueryResponse rsp) throws Exception {
         //Check: https://cwiki.apache.org/confluence/display/solr/Uploading+Data+with+Index+Handlers
         //TODO: Handle the request
+
+        //1) Parse input: Graphite line protocol, opentsdb, influxdb
+
         LOGGER.info("I was called");
         String result = IOUtils.toString(req.getContentStreams().iterator().next().getStream(), "UTF-8");
         LOGGER.info("Content is: {}", result);
+
+        //2) Converter to time series
+        MetricTimeSeries ts = new MetricTimeSeries.Builder("cpu_load_short")
+                .attribute("host","server02")
+                .attribute("region","us-west")
+                .point(Instant.now().toEpochMilli(), 2061d)
+                .build();
+
+        KassiopeiaSimpleConverter converter = new KassiopeiaSimpleConverter();
+        BinaryTimeSeries tsB = converter.to(ts);
+
+        SolrInputDocument doc = new SolrInputDocument();
+
+        tsB.getFields().forEach(doc::addField);
+
+        //3) add to solr
+        //generate uuid
+        req.getCore();
+
     }
 
     @Override
